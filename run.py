20bfaa643da2ae4f603268ec8217a3589239d8b6
Cesargarciajr <>
2023-07-04 13:13:52 +0000

    Add main function to call other functions and start the game also fixed bug when not valid answer in inputted by user

--- run.py
+++ run.py
@@ -7,57 +7,55 @@ def get_user_input():
     Function to get user input and transform the input into one of the valid options
     if not valid will print an error and restart the function
     """
-
     while True:
-        user = input("\nSelect (R) for rock, (P) for Paper or (S) for Scissors: ")
+        user = input("\nSelect (R) for Rock, (P) for Paper or (S) for Scissors: ")
         if user == "r" or user == "R":
             user = "rock"
-            print(f'user choice: {user}')
-            get_computer_choice()
             break
         elif user == "p" or user == "P":
             user = "paper"
-            print(f'user choice: {user}')
-            get_computer_choice()
             break
         elif user == "s" or user == "S":
             user = "scissors"
-            print(f'user choice: {user}')
-            get_computer_choice()
             break
         else:
-            print("\nError! Please select one of the valid options.")
+            print("\nERROR! Please select one of the valid options.")
+    print(f'You choose: {user}')
+    return user
 
 def get_computer_choice():
     """
     Function to randomly gerenate between computer choice
     """
-
     options = ("rock", "paper", "scissors")
     computer = random.choice(options)
-    print(f'computer choice: {computer}')
-    result(computer, user)
+    print(f'Computer choose: {computer}')
+    return computer
 
 def result(user, computer):
-    """
-    Function to compare the choices, print the result and count the rounds
-    """
-    round = 0
-    while round <= 5 :
-        if user == computer:
-            print("its a tie!Let's try again")
-            round +1
-            get_user_input()
-        elif user == "rock" and computer == "scissors":
-            print("You win!")
-            round +1
-        elif user == "paper" and computer == "rock":
-            print("You win!")
-            round +1
-        elif user == "scissors" and computer == "paper":
-            print("You win!")
-            round +1
-        else:
-            print("You Lose!")
-            round +1
+    if user == computer:
+        print("its a tie!")
+    elif user == "rock" and computer == "scissors":
+        print("You win!")
+    elif user == "paper" and computer == "rock":
+        print("You win!")
+    elif user == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You Lose!")
+    
+def main():
+    print("\nWelcome to THE ULTIMATE RPS Game!")
+    print("""
+        _______             ______                 _______
+    ---'   ____)        ---'  ____)____        ---'   ____)____
+          (_____)                ______)                 ______)
+          (_____)                _______)            __________)
+          (____)                _______)             (____)
+    ---.__(___)         ---.__________)        ---.__(___)
+        """)
+    user_choice = get_user_input()
+    computer_choice = get_computer_choice()
+    result(user_choice, computer_choice)
 
+main()
