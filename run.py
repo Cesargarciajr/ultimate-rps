91291bada5c46e2ea6171b93d633096118eb1db5
Cesargarciajr <>
2023-07-04 14:18:00 +0000

    Add figures to computer and user choise as well as win or lose result

--- run.py
+++ run.py
@@ -11,16 +11,37 @@ def get_user_input():
         user = input("\nSelect (R) for Rock, (P) for Paper or (S) for Scissors: ")
         if user == "r" or user == "R":
             user = "rock"
+            rock_fig = print(""" \nYou choose: Rock
+                  _______
+              ---'   ____)
+                    (_____)
+                    (_____)
+                    (____)
+              ---.__(___)""")
             break
         elif user == "p" or user == "P":
             user = "paper"
+            paper_fig = print(""" \nYou choose: Paper
+                  ________
+              ---'    ____)____
+                         ______)
+                        _______)
+                       _______)
+              ---.__________)
+""")
             break
         elif user == "s" or user == "S":
             user = "scissors"
+            scissors_fig = print(""" \nYou choose: Scissors
+                  _______
+              ---'   ____)____
+                        ______)
+                    __________)
+                    (____)
+              ---.__(___) """)
             break
         else:
             print("\nERROR! Please select one of the valid options.")
-    print(f'You choose: {user}')
     return user
 
 def get_computer_choice():
@@ -29,23 +50,79 @@ def get_computer_choice():
     """
     options = ("rock", "paper", "scissors")
     computer = random.choice(options)
-    print(f'Computer choose: {computer}')
+    if computer == "rock":
+        print("""\nComputer chose: Rock!
+                 ________
+                (____)   '---
+               (_____)
+               (_____)
+                (____)
+                 (___)__.---""")
+    elif computer == "paper":
+        print("""\nComputer chose: Paper!
+                 _______
+            ____(____   '---
+           (______
+          (_______
+          (_______
+             (__________.---""")
+    else:
+        print("""\nComputer chose: Scissors!
+                _______
+           ____(____   '---
+          (______
+          (__________
+               (____)
+                (___)__.---""") 
+
     return computer
 
 def result(user, computer):
     if user == computer:
         print("its a tie!")
     elif user == "rock" and computer == "scissors":
-        print("You win!")
+        print("""\nYou win!
+                  ︵  
+                 |  |
+              ---'  '_____
+                    (_____)
+                    (_____)
+                    (____)
+              ---.__(___)""")
     elif user == "paper" and computer == "rock":
-        print("You win!")
+        print("""\nYou win!
+                  ︵  
+                 |  |
+              ---'  '_____
+                    (_____)
+                    (_____)
+                    (____)
+              ---.__(___)""")
     elif user == "scissors" and computer == "paper":
-        print("You win!")
+        print("""\nYou win!
+                  ︵  
+                 |  |
+              ---'  '_____
+                    (_____)
+                    (_____)
+                    (____)
+              ---.__(___)""")
     else:
-        print("You Lose!")
+                print("""\nYou Lose!
+             _________
+            (____)    '---
+            (_____)
+            (_____)
+             (____) __.---
+                '  '
+                |  |
+                 ︶        """)
     
 def main():
-    print("\nWelcome to THE ULTIMATE RPS Game!")
+    print("""\n                     Welcome to THE ULTIMATE RPS Game!
+             \nChallenge the computer for a 5 round game and see if you can beat the Computer BEAST.
+                Win 3 times in a row to unlock secret!")
+             \n                         Good Luck and have fun!""")
     print("""
         _______             ______                 _______
     ---'   ____)        ---'  ____)____        ---'   ____)____
