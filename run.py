b5ca5de9b05fb25d292f9065778b3cb14c8a7e73
Cesargarciajr <>
2023-07-04 12:17:48 +0000

    Add function to generate computer choice

--- run.py
+++ run.py
@@ -11,11 +11,25 @@ def get_user_input():
         user = input("\nSelect (R) for rock, (P) for Paper or (S) for Scissors: ")
         if user == "r" or user == "R":
             user = "rock"
-        elif user == "p" or user =="P":
+            get_computer_choice()
+            break
+        elif user == "p" or user == "P":
             user = "paper"
+            get_computer_choice()
+            break
         elif user == "s" or user == "S":
             user = "scissors"
+            get_computer_choice()
+            break
         else:
             print("\nError! Please select one of the valid options.")
-    
+
+def get_computer_choice():
+    """
+    Function to randomly choose between options and play as computer
+    """
+    options = ("rock", "paper", "scissors")
+    computer = random.choice(options)
+    print(computer)
+
 get_user_input()
